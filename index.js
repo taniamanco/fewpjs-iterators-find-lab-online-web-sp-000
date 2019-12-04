6a34952087cb38e2f0d906e8cc8215968691d62e
Tania Manco <belalov.tania16@gmail.com>
2019-12-04 15:32:13 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+superbowlWin = (arr) => {
+  let result = arr.find( arr => arr.result === "W" )
+  return !!result ? result.year : undefined
+}
